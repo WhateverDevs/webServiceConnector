c56646d212720146f46c994e18a97f0d039fd01e
Rodrigo Varga <varguiniano@gmail.com>
2020-04-22 08:53:26 +0200

    - Default installer.

---
+++ Runtime/NonAuth/NonAuthServiceConnectorInstaller.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using Zenject;
+
+namespace WhateverDevs.WebServiceConnector.Runtime.NonAuth
+{
+    /// <summary>
+    /// Installer for the non auth webservice connector.
+    /// </summary>
+    [CreateAssetMenu(menuName = "WhateverDevs/WebServiceConnector/NonAuthInstaller",
+                     fileName = "NonAuthServiceConnectorInstaller")]
+    public class NonAuthServiceConnectorInstaller : ScriptableObjectInstaller
+    {
+        /// <summary>
+        /// Install the connector to all that ask for the interface.
+        /// </summary>
+        public override void InstallBindings() =>
+            Container.Bind<INonAuthWebServiceConnector>().To<NonAuthServiceConnector>().AsSingle().Lazy();
+    }
+}
